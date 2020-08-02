0c4c3ce7ff09cca8608a3277a5fae573c941b3f4
begumsacak <63175082+begumsacak@users.noreply.github.com>
2020-08-02 09:00:31 -0500

    fixed db syntax and started adding functions to server.js

--- employee-tracker/db/seed.sql
+++ employee-tracker/db/seed.sql
@@ -1,5 +1,8 @@
-INSERT into department (name) VALUES ("Human Resources", "Finance", "Marketing");
+INSERT into department (name) VALUES ("Human Resources"), ("Finance"), ("Marketing");
 
-INSERT into role (title, salary, department_id) VALUES ("Manager", "100000", "1"), ("Human Resources Specialist", "50000", "2"), ("Human Resources Analyst", "30000", "2"), ("Marketing Specialist", "50000", "3");
+INSERT into role (title, salary, department_id) VALUES ("Manager", 10000.00, 1), 
+("Human Resources Specialist", 50000.00, 2),
+ ("Human Resources Analyst", 30000.00, 2),
+ ("Marketing Specialist", 50000.00, 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Smith", "1", "1"), ("Mary", "Williams", "2", null), ("Robert", "Miller", "3", null), ("Michael", "Davis", "4", null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Smith", 1, 1), ("Mary", "Williams", 2, null), ("Robert", "Miller", 3, null), ("Michael", "Davis", 4, null);
--- employee-tracker/server.js
+++ employee-tracker/server.js
@@ -7,12 +7,13 @@ const connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "Ilhanirem51",
-    database: "employee_db"
+    database: "employees_db"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    manageEmployees();
 });
 
 function manageEmployees() {
@@ -24,24 +25,78 @@ function manageEmployees() {
                 type: "list",
                 choices: [
                     "Add an Employee",
-                    "View Employees",
-                    "Remove an Employee",
+                    "View Employee",
+                    "Update an Employee",
+                    "Add a Role",
+                    "Add a Department",
+                    "View Department",
                     "Exit",
                 ],
             },
         ])
         .then((answer) => {
-            switch (answer.manageEmployees) {
+            switch (answer.mainMenu) {
                 case "Add an Employee":
                     addEmployee();
                     break;
                 case "View Employee":
                     viewEmployee();
                     break;
-                case "Remove an Employee":
-                    removeEmployee();
+                case "Update an Employee":
+                    updateEmployee();
                     break;
+                case "Add a Role":
+                    addRole();
+                    break;
+                case "Add a Department":
+                    addDepartment();
+                    break;
+                case "View Department":
+                    viewDepartment();
+                    break;
+                default:
+                    connection.end();
+                    process.exit();
             }
         });
 }
+function viewEmployee() {
+    connection.query("SELECT * FROM employee", (err, results) =>{
+        console.table(results)
+        manageEmployees();
+    })
+}
 
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                name: "firstName",
+                message: "What is your first name?",
+                type: "input"
+            },
+            {
+                name: "lastName",
+                message: "What is your last name?",
+                type: "input"
+            },
+            {
+                name: "roleID",
+                message: "What is your role ID?",
+                type: "input"
+            },
+            {
+                name: "managerID",
+                message: "What is your manager ID?",
+                type: "input"
+            },
+        ])
+        //The placeholders can be replaced with the input
+        //Double question marks: table names or column names
+        .then((answer) => {
+            connection.query("INSERT into employee(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.firstName, answer.lastName, answer.roleID, answer.managerID], (err)=> {
+               console.log("New employee has been added")
+               manageEmployees()
+            } )
+        })
+}
